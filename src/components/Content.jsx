284074f39c8ee15356e0c91414d8adfd5e9eda81
Federico Schianchi <federicoschianchi2004@gmail.com>
2023-05-08 10:22:17 +0200

    search bar

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,6 +4,7 @@ import Spinner from "react-loading-indicators";
 function Content() {
   const [data, setData] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const [searchInput, setSearchInput] = useState("")
 
     useEffect(() => {
       setIsLoading(true)
@@ -14,10 +15,18 @@ function Content() {
         })
         .finally(() => setIsLoading(false))
     }, [])
-    console.log(data)
+    let stableData = data
+
     if (isLoading) {
       return <div className="flex justify-center"><Spinner style={{ fontSize: "20px" }} color="pink" /></div>;
     }
+
+    const handleChange = (e) => {
+      e.preventDefault();
+      setSearchInput(e.target.value);
+    };
+    
+   let filteredData = searchInput ? stableData.filter((p) => p.name.toLowerCase().includes(searchInput.toLowerCase())) : stableData;
     
   return (
     <>
@@ -27,12 +36,11 @@ function Content() {
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                 <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
             </div>
-            <input type="search" id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#4F45E4] focus:border-[#4F45E4] dark:bg-[#ebebec] dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-[#4F45E4] dark:focus:border-[#4F45E4]" placeholder="Search champion, characters..." required/>
-            <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-[#4F45E4] hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-[#4F45E4] dark:hover:bg-[#4F45E4] dark:focus:ring-[#4F45E4]">Search</button>
+            <input type="search" onChange={handleChange} value={searchInput} id="default-search" className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-[#4F45E4] focus:border-[#4F45E4] dark:bg-[#ebebec] dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-[#4F45E4] dark:focus:border-[#4F45E4]" placeholder="Search champion, characters name..." required/>
         </div>
     </form>
-     <div className="p-10 grid grid-cols-4 gap-4">
-      {data.map((p) => (
+     <div className="p-10 grid grid-cols-4 gap-4 min-h-full">
+      {filteredData.map((p) => (
         <div key={p.id} className="max-w-sm rounded overflow-hidden shadow-lg">
         <img className="w-full" src={p.image} alt="lgbt"/>
         <div className="px-6 py-4">
